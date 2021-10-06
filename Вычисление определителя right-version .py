9b22522502931e194ccad2d76274fd4cb13ff91a
Memori707 <74479353+Memori707@users.noreply.github.com>
2021-10-06 15:10:45 +0300

    Update Вычисление определителя right-version .py

--- Вычисление определителя right-version .py
+++ Вычисление определителя right-version .py
@@ -1,6 +1,3 @@
-def getMatrixMinor(m,i,j):
-    return [row[:j] + row[j+1:] for row in (m[:i]+m[i+1:])]
-
 def determinant(matr):
     #print('Произошла рекурсия')
     size = len(matr)
@@ -8,7 +5,7 @@ def determinant(matr):
         return matr[0][0]
     for k in range(size):
         #print(f"Работаем с {k} элементом строки")
-        t = getMatrixMinor(matr,0,k)
+        t = [ row[:k] + row[k+1:] for row in (matr[1:])]
         #print(t)
         matr[0][k] = matr[0][k] * (-1)**(k) * determinant(t)
     return sum(matr[0])
