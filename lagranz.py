be0b5444b52e5c78d018e833f52352ca416dc5d5
Memori707 <74479353+Memori707@users.noreply.github.com>
2021-11-18 07:13:52 +0300

    Update lagranz.py

--- lagranz.py
+++ lagranz.py
@@ -4,20 +4,22 @@ def lagranz(x, y):
     t = symbols('t')
     z = 0
     for j in range(len(y)):
-        p1 = 1;
-        p2 = 1;
+        numerator = 1;
+        denominator = 1;
         for i in range(len(x)):
             if i == j:
-                p1 = p1 * 1;
-                p2 = p2 * 1
+                numerator = numerator * 1;
+                denominator = denominator * 1
             else:
-                p1 = expand(p1 * (t - x[i]))
-                p2 = p2 * (x[j] - x[i])
-        z = expand(z + y[j] * p1 / p2)
+                numerator = expand(numerator * (t - x[i]))
+                denominator = denominator * (x[j] - x[i])
+        z = expand(z + y[j] * numerator / denominator)
     f_x = lambdify(t, z)
     output = []
     for k in range(len(x)):
         output.append([x[k],y[k],f_x(x[k])])
     return (output, z)
+
    
-   
+# Вывод в формате ([[x1,y1,f_x1]...[xi,yi,f_xi]], f_x) 
+# f_x - строка, можно преобразовать в функцию типа f_x(x0) = y0  
