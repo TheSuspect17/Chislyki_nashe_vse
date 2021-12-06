fe3c71702c3cd33c81c0a0cd97a79a819336c0b0
Memori707 <74479353+Memori707@users.noreply.github.com>
2021-12-06 14:02:24 +0300

    Create Ввод функций.py

---
+++ Ввод функций.py
@@ -0,0 +1,19 @@
+def input_function():
+    t = symbols('t')
+    y = symbols('y')
+    z = symbols('z')
+    equation = []
+
+    qwe = eval(input('Введите y` [доступные символы t,y,z]: '))
+    equation.append(qwe)
+    global answer
+    answer = input('Хотите ввести еще одно уравнение? [1/0] ')
+    if answer == '1':
+        qwe = eval(input('Введите z` [доступные символы t,y,z]: '))
+        equation.append(qwe)
+    function = []
+    for i in range(len(equation)):
+        function.append(lambdify([t, y, z], equation[i]))
+    return function
+ 
+# Может багаться
