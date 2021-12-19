a595983f1933a468f9fb8c445c46d8e9fbf1d098
Memori707 <74479353+Memori707@users.noreply.github.com>
2021-12-19 19:55:27 +0300

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -825,3 +825,106 @@ def method_ant(distmat, alpha=0.6, beta=0.65, pheEvaRate=0.3):
         # plt.show()
         iter += 1
     return (min(length),antPath[length.index(min(length))] )
+
+
+def TSP_SA(G):
+    s = list(range(len(G))) 
+    c = cost(G, s)
+    ntrial = 1
+    T = 30
+    alpha = 0.99
+    while ntrial <= 1000:
+        n = np.random.randint(0, len(G))
+        while True:
+            m = np.random.randint(0, len(G))
+            if n != m:
+                break
+        s1 = swap(s, m, n)
+        c1 = cost(G, s1)
+        if c1 < c:
+            s, c = s1, c1
+        else:
+            if np.random.rand() < np.exp(-(c1 - c)/T):
+                s, c = s1, c1
+        T = alpha*T
+        ntrial += 1
+    return(s,c)
+        
+def swap(s, m, n):
+    i, j = min(m, n), max(m, n)
+    s1 = s.copy()
+    while i < j:
+        s1[i], s1[j] = s1[j], s1[i]
+        i += 1
+        j -= 1
+    return s1
+  
+def cost(G, s):
+    l = 0
+    for i in range(len(s)-1):
+        l += G[s[i]][s[i+1]]
+    l += G[s[len(s)-1]][s[0]] 
+    return l
+def copyToFinal(curr_path):
+    final_path[:N + 1] = curr_path[:]
+    final_path[N] = curr_path[0]
+
+def firstMin(adj, i):
+    min_ = maxsize
+    for k in range(N):
+        if adj[i][k] < min_ and i != k:
+            min_ = adj[i][k]
+    return min_
+
+def secondMin(adj, i):
+    first, second = maxsize, maxsize
+    for j in range(N):
+        if i == j:
+            continue
+        if adj[i][j] <= first:
+            second = first
+            first = adj[i][j]
+        elif(adj[i][j] <= second and
+            adj[i][j] != first):
+            second = adj[i][j]
+    return second
+
+def TSPRec(adj, curr_bound, curr_weight, level, curr_path, visited):
+    global final_res
+    if level == N:
+        if adj[curr_path[level - 1]][curr_path[0]] != 0:
+            curr_res = curr_weight + adj[curr_path[level - 1]][curr_path[0]]
+            if curr_res < final_res:
+                copyToFinal(curr_path)
+                final_res = curr_res
+        return
+
+    for i in range(N):
+        if (adj[curr_path[level-1]][i] != 0 and visited[i] == False):
+            temp = curr_bound
+            curr_weight += adj[curr_path[level - 1]][i]
+            if level == 1:
+                curr_bound -= ((firstMin(adj, curr_path[level - 1]) + firstMin(adj, i)) / 2)
+            else:
+                curr_bound -= ((secondMin(adj, curr_path[level - 1]) + firstMin(adj, i)) / 2)
+            if curr_bound + curr_weight < final_res:
+                curr_path[level] = i
+                visited[i] = True
+                TSPRec(adj, curr_bound, curr_weight, level + 1, curr_path, visited)
+            curr_weight -= adj[curr_path[level - 1]][i]
+            curr_bound = temp
+            visited = [False] * len(visited)
+            for j in range(level):
+                if curr_path[j] != -1:
+                    visited[curr_path[j]] = True
+
+def TSP(adj):
+    curr_bound = 0
+    curr_path = [-1] * (N + 1)
+    visited = [False] * N
+    for i in range(N):
+        curr_bound += (firstMin(adj, i) + secondMin(adj, i))
+    curr_bound = math.ceil(curr_bound / 2)
+    visited[0] = True
+    curr_path[0] = 0
+    TSPRec(adj, curr_bound, 0, 1, curr_path, visited)
